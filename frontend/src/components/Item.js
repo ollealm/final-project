b7a3c268e57014ab7800c9638203dcd1d581ad27
Olle Alm <olle.alm@me.com>
2020-06-08 14:58:44 +0200

    Item route and component, itemsList to redux and ScrollToTop Component

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { items as itemsReducer } from "../reducers/items"
 
-export const Item = ({ item }) => {
+
+export const Item = ({ itemProps }) => {
+  const [item, setItem] = useState()
+  const { itemNumber } = useParams();
+
+  const itemsArray = useSelector(store => store.items.itemsArray)
+  console.log("itemsArray from store")
+  console.log(itemsArray)
+  console.log(itemsArray.length)
+
+  if (itemsArray.length > 0 && !item) {
+    let singleItem = itemsArray.find(({ number }) => number === +itemNumber)
+    console.log("find")
+    console.log(singleItem)
+    setItem(singleItem)
+  }
+
+
+  // if (itemsArray.length > 0) {
+  //   setItem(itemsArray.find(({ number }) => number === +itemNumber))
+  //   console.log("find")
+  //   console.log(item)
+  // } else console.log("no items in array")
+
+
+  const url = `http://localhost:8090/items/${itemNumber}`;
+  console.log(itemNumber)
+
+  useEffect(() => {
+    // setLoading(true); //change to dispach
+    console.log("useEffect")
+    console.log(itemsArray.length)
+    if (itemsArray.length === 0) {
+      console.log("fetching")
+      fetch(url)
+        .then((res) => {
+          return res.json();
+        })
+        .then(data => {
+          console.log("Data: ", data)
+          setItem(data)
+          console.log("Item after set: ", item)
+          // setTimeout(() => { // timeout to always show loader
+          // setItems(data.items);
+          // setPages({ pages: data.pages, total: data.results })
+          // setLoading(false)
+          // }, 50)
+        });
+    } else console.log("Item already in store")
+  }, []);
+
+  // const saveProduct = () => {
+  //   dispatch(itemsReducer.actions.saveItem(product))
+  // }
+
   return (
     <div>
+      Item {itemNumber}
+      {console.log("loading component")}
+      {console.log("Item: ", item)}
       {item && <div>
+        {console.log("loading item")}
         <h2>Singel item</h2>
         <h3>{item.number} {item.name}</h3>
         <p>{item.group}</p>
--- frontend/src/pages/ListItems.js
+++ frontend/src/pages/ListItems.js
@@ -4,17 +4,22 @@ import { useParams, Link } from "react-router-dom";
 import { Item } from "../components/Item"
 import { LoadingIndicator } from '../lib/LoadingIndicator';
 //import { Loading } from '../components/Loading';
+import { useDispatch, useSelector } from 'react-redux';
+import { items as itemsReducer } from "../reducers/items"
 
 export const ListItems = () => {
   const [items, setItems] = useState([]);
   const [itemIndex, setItemIndex] = useState(0);
   const [pages, setPages] = useState({});
   const [loading, setLoading] = useState(true)
-  const url = "http://localhost:8090/items";
+
+  const dispatch = useDispatch();
+
+  const url = "http://localhost:8090/items?page=1";
 
   const { itemNumber } = useParams();
 
-
+  // Move to Thunk
   useEffect(() => {
     setLoading(true); //change to dispach
     console.log("getting data")
@@ -23,11 +28,15 @@ export const ListItems = () => {
         return res.json();
       })
       .then(data => {
-        setTimeout(() => { // timeout to always show loader
-          setItems(data.items);
-          setPages({ pages: data.pages, total: data.results })
-          setLoading(false)
-        }, 50)
+        setItems(data.items);
+        //dispach data.items to Store
+        dispatch(itemsReducer.actions.saveItem(data.items))
+        console.log("dispatching items")
+        // console.log(data.items)
+        setPages({ pages: data.pages, total: data.results })
+        setLoading(false)
+        // setTimeout(() => { // timeout to always show loader
+        // }, 50)
       });
   }, []);
 
@@ -55,7 +64,7 @@ export const ListItems = () => {
         )
       }
       {/* <Loading loading={loading} /> -> ui*/}
-      <Item item={items[itemIndex]} />
+      {/* <Item item={items[itemIndex]} /> */}
     </div>
   )
 }
