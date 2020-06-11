0b0f558b3d173793b4151fa3546be8e6c8d8c6ae
Olle Alm <Olle@Olles-MacBook-Pro-2.local>
2020-06-11 14:46:14 +0200

    clean up, remove unsed code

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,17 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import bcrypt from "bcrypt-nodejs"
 
-import foodData from './data/livsmedel_mini.json'
-// foodData = foodData.replace('/(?<=\d),(?=\d)/gm', '.')
-let items = foodData.LivsmedelDataset.LivsmedelsLista.Livsmedel
-
-
 import User from './models/users'
 import Item from './models/items'
 
+import foodData from './data/livsmedel_mini.json'
+let items = foodData.LivsmedelDataset.LivsmedelsLista.Livsmedel
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mutrientsAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 mongoose.set('useCreateIndex', true)
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8090
 const app = express()
 
@@ -29,6 +22,8 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+///// RESET DATABASE /////
 
 if (process.env.RESET_DB) {
   console.log("Database reset")
@@ -49,13 +44,11 @@ if (process.env.RESET_DB) {
         };
       }, initialValue);
     };
-    // Change Varde from ie 5 000,0 to 5000.0
+    // Change 'Varde' from ie 5 000,0 to 5000.0
     items = items.map((item) => {
       item.Naringsvarden.Naringsvarde = item.Naringsvarden.Naringsvarde.map(item => {
         item.Varde = item.Varde.replace(/(?<=\d),(?=\d)/gm, '.')
         item.Varde = item.Varde.replace(/(?<=\d)\s(?=\d)/gm, '')
-        // item.Varde = item.Varde.replace(',', '.')
-        // item.Varde = item.Varde.replace(' ', '')
         item.Forkortning = item.Forkortning.replace(/\:/g, '_')
         item.Forkortning = item.Forkortning.replace("\-", '_')
         item.Forkortning = item.Forkortning.replace("\/", '_')
@@ -64,9 +57,9 @@ if (process.env.RESET_DB) {
       item.Naringsvarden.Naringsvarde = convertArrayToObject(item.Naringsvarden.Naringsvarde, "Forkortning")
       return item
     })
+
     console.log("Pre-processing")
     console.timeLog("Reset")
-    // console.log(items[0])
 
     await items.forEach((item, index) => {
       new Item({
@@ -94,6 +87,7 @@ if (process.env.RESET_DB) {
 
 
 
+///// AUTHENTICATOR MIDDLEWARE /////
 
 const authenticator = async (req, res, next) => {
   try {
@@ -114,17 +108,18 @@ const authenticator = async (req, res, next) => {
 }
 
 
-///// Routes /////
+///// ROUTS /////
+
+//ROOT
 app.get('/', (req, res) => {
   res.send('Hello world')
+  // Display routes
 })
 
 
 ///// Aggregate /////
-
 app.get('/nutrients', async (req, res) => {
   const { name, group, sort, nutrient, nut, ratio, page = 1, limit = Infinity } = req.query
-
   let items = []
 
   if (name) {
@@ -187,9 +182,6 @@ app.get('/nutrients', async (req, res) => {
       .sort({ numberOfItems: 1 })
   }
 
-  // const nutrient = "VitC"
-  // const ratio = "Ener"
-
   console.log("starting aggregate")
 
   if (ratio) {
@@ -201,7 +193,6 @@ app.get('/nutrients', async (req, res) => {
           _id: "$name",
           ratio: {
             // $divide: [`$nutrients.${nutrient}.Varde`, `$nutrients.${ratio}.Varde`]
-
             $cond: [{ $eq: [`$nutrients.${ratio}.Varde`, 0] }, `Infinite`, { $round: [{ $divide: [`$nutrients.${nutrient}.Varde`, `$nutrients.${ratio}.Varde`] }, 4] }]
           },
           unit_ratio: { $concat: [`$nutrients.${nutrient}.Enhet`, "/", `$nutrients.${ratio}.Enhet`] },
@@ -265,16 +256,10 @@ app.get('/items', async (req, res) => {
     });
   }
   const sortQuery = (sort) => {
-    if (sort === 'name') return { name: 1 }
-    if (sort === 'group') return { group: 1 }
     if (sort === 'nutrient') return { [`nutrients.${nutrient}.Varde`]: -1 }
+    return { [sort]: 1 }
   }
   databaseQuery.sort(sortQuery(sort));
-  /*
-    databaseQuery = databaseQuery.sort({
-      [sort]: order
-    });
-  */
 
   console.log("2")
   console.timeLog("get")
@@ -289,7 +274,6 @@ app.get('/items', async (req, res) => {
   console.timeLog("get")
 
   if (items.length > 0) {
-    // res.json({ page, pages, results, items })
     res.json({ page, pages, results, items })
   } else {
     res.status(400).json({ error: "No items found" })
@@ -311,97 +295,6 @@ app.get('/items/:id', async (req, res) => {
 
 
 
-// ITEMS OLD
-app.get('/itemsOLD', (req, res) => {
-  const { name, group, language, minpages = 0, maxpages = Infinity, rating, sort, nutrient, ratio, page, test, test2, limit = 20 } = req.query
-
-  console.log(req.query)
-
-  const filterItems = (array, filter) => {
-    return array.toString().toLowerCase().includes(filter)
-  }
-
-  let filteredItems = items;
-
-  //Filter
-  if (name) {
-    filteredItems = filteredItems.filter(item => filterItems(item.Namn, name))
-  }
-  if (group) {
-    filteredItems = filteredItems.filter(item => filterItems(item.Huvudgrupp, group))
-  }
-  if (test) {
-    filteredItems = filteredItems.filter(item => item.Naringsvarden.Naringsvarde.length > +test)
-  }
-  if (test2) {
-    filteredItems = filteredItems.filter(item => item.Naringsvarden.Naringsvarde.includes())
-  }
-
-  //Sort
-  //sorting on name and group was a lot more complicated that I assumed 
-  if (sort) {
-    if (sort === 'name') {
-      filteredItems = filteredItems.sort((a, b) => {
-        const nameA = a.Namn.toLowerCase()
-        const nameB = b.Namn.toLowerCase()
-        if (nameA < nameB) return -1;
-        if (nameA > nameB) return 1;
-        return 0;
-      });
-    } else if (sort === 'group') {
-      filteredItems = filteredItems.sort((a, b) => {
-        const groupA = a.Huvudgrupp.toLowerCase()
-        const groupB = b.Huvudgrupp.toLowerCase()
-        if (groupA < groupB) return -1;
-        if (groupA > groupB) return 1;
-        return 0;
-      });
-    } else if (sort === 'nutrient') {
-      filteredItems = filteredItems.sort((a, b) => {
-        const valueA = parseFloat(a.Naringsvarden.Naringsvarde[nutrient].Varde.replace(',', '.'))
-        const valueB = parseFloat(b.Naringsvarden.Naringsvarde[nutrient].Varde.replace(',', '.'))
-        return valueB - valueA
-        // if (valueA < valueB) return 1;
-        // if (valueA > valueB) return -1;
-        // return 0;
-      })
-    } else if (sort === 'ratio') {
-      filteredItems = filteredItems.sort((a, b) => {
-        const nutrientA = parseFloat(a.Naringsvarden.Naringsvarde[nutrient].Varde.replace(',', '.'))
-        const denominatorA = parseFloat(a.Naringsvarden.Naringsvarde[ratio].Varde.replace(',', '.'))
-        const nutrientB = parseFloat(b.Naringsvarden.Naringsvarde[nutrient].Varde.replace(',', '.'))
-        const denominatorB = parseFloat(b.Naringsvarden.Naringsvarde[ratio].Varde.replace(',', '.'))
-        const ratioA = nutrientA / denominatorA || 0
-        const ratioB = nutrientB / denominatorB || 0
-        return ratioB - ratioA
-      })
-      /*
-            filteredItems = filteredItems.map(item => {
-              // console.log(item.Naringsvarden.Naringsvarde[nutrient].Varde)
-              const nutrientValue = parseFloat(item.Naringsvarden.Naringsvarde[nutrient].Varde.replace(',', '.'))
-              const denominatorValue = parseFloat(item.Naringsvarden.Naringsvarde[ratio].Varde.replace(',', '.'))
-              const ratioValue = nutrientValue / denominatorValue
-              return ratioValue
-            })
-      */
-    }
-  }
-  let results = filteredItems.length
-  let pages = Math.ceil(results / limit)
-  //Pagination
-  if (page) {
-    filteredItems = filteredItems.slice((page - 1) * limit, limit * page)
-  }
-  //Returning results 
-  console.log("filtered return " + filteredItems.length)
-  if (filteredItems.length > 0) res.json({ results, pages, filteredItems })
-  else res.status(404).json({ message: 'No Items found' })
-})
-
-
-
-
-
 
 ///// USER ROUTES /////
 
@@ -413,7 +306,6 @@ app.post("/users", async (req, res) => {
     const user = new User({ name, password: bcrypt.hashSync(password) }) // stores password encrypted
     const saved = await user.save()
     res.status(201).json({ userId: saved._id, accessToken: saved.accessToken });
-    // res.status(201).json(saved) // don't return password
   } catch (err) {
     res.status(400).json({ message: "could not save user", errors: err })
   }
@@ -458,6 +350,7 @@ app.post("/users/:id", async (req, res) => {
       { $push: { savedItems: { item, itemNumber, price } } },
       { useFindAndModify: false },
     )
+    // .populate('savedItems') populate items
     res.status(201)
       .json(user.savedItems) // returns before incremented, I could not fix this.
   } catch (err) {
